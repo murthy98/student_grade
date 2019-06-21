89c73e7cbc6494d7c6c191907a4dd635bfc5a1f8
Murthy  G <narayanamurthy.gidugu@gmail.com>
2019-06-21 14:45:33 +0530

    db

--- dbconnection.py
+++ dbconnection.py
@@ -1,8 +1,7 @@
 import psycopg2
 import os
 def connection():
-    #db = f"dbname={os.getenv('DATABASE')} user={os.getenv('USER')} password={os.getenv('PASSWORD')} host={os.getenv('HOST')}"
-    conn=psycopg2.connect(database='d3aooc4t84kn0n' ,user='cdzgrdnckdgqjw' ,password='032ea1b1f89bb5b4d594fe27e510b10531ebaee8bd3d4f0947c8297bcecdca14' ,host='ec2-23-21-186-85.compute-1.amazonaws.com')
-    #conn = psycopg2.connect(db)
+    db = f"dbname={os.getenv('DATABASE')} user={os.getenv('USER')} password={os.getenv('PASSWORD')} host={os.getenv('HOST')}"
+    conn = psycopg2.connect(db)
     c = conn.cursor()
     return c, conn
